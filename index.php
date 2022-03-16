29152d068b5d761cfa7a6907e36659a140e026ba
Thomas <venturini.thomas95@gmail.com>
2022-03-16 16:35:01 +0100

    .

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
-
 <?php
 // Riscrivere questa pagina del sito google https://policies.google.com/faq.
 // Ci sono diverse domande con relative risposte.
 // Gestire il “Database” e la visualizzazione di queste domande e risposte con PHP.
+
+
     
 ?>
 
@@ -12,9 +13,31 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./style.css">
     <title>Document</title>
 </head>
 <body>
+    <header>
+
+        <div class="header-top">
+            <img class="logo" src="./img/Google-Logo.png" alt="Google Logo">
+            <h4>Privacy e termini</h4>
+        </div>
+
+        <div class="header-bottom">
+            <ul>
+                <li><a href='#'>Introduzione</a></li>
+                <li><a href='#'>Norme sulla privacy</a></li>
+                <li><a href='#'>Termini di servizio</a></li>
+                <li><a href='#'>Tecnologie</a></li>
+                <li><a href='#'>Domande frequenti</a></li>
+            </ul>
+
+        </div>
+    </header>
     
 </body>
 </html>
